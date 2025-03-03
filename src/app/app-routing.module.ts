f8e18b1b7ad31c7cb9470c7f8217ef3842377eef
Eduardo Sartori <eddudrux@gmail.com>
2025-03-03 12:59:43 -0300

    feat(router): adiciona rotas para login e gerenciamento de produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ProductFormComponent } from './product/product-form/product-form.component';
+import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: ProductListComponent, canActivate: [AuthGuard] },
+  { path: 'product/edit/:id', component: ProductFormComponent, canActivate: [AuthGuard] },
+  { path: 'product/add', component: ProductFormComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    AuthModule, 
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
